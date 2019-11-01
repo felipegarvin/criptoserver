bd1c0f248eb47907cf6963b0307054fabc1ea65f
felipe garvin carvalho <fehaha6@outlook.com>
2019-11-01 12:09:00 -0300

    init

--- criptoserver/Server.cs
+++ criptoserver/Server.cs
@@ -8,6 +8,22 @@ namespace criptoserver
 {
     public class Server
     {
-        public string Teste { get; set; }
+        static void Main()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Public\TestFolder\WriteText.txt");
+
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\WriteLines2.txt");
+
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
     }
 }
